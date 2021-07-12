bfe812c4fa20f56dfce0ed6510b6dc22ce9acfa8
Cedric Nicoloso <cedric.nicoloso@gmail.com>
2021-07-12 22:39:31 +0200

    Init project :zap:

---
+++ global-bottom.vue
@@ -0,0 +1,6 @@
+<template>
+  <footer v-if="$slidev.nav.currentLayout !== 'cover'" class="absolute bottom-0 left-1 p-2">
+    Cedric Nicoloso - LyonJS #68
+    {{$slidev.nav.currentLayout}}
+  </footer>
+</template>
